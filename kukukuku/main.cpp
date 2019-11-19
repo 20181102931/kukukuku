468e0019ce9a6e1b67982753119b9dac26491541
20181102931 <18647197975@163.com>
2019-11-19 17:37:38 +0800

    11
    12

--- kukukuku/main.cpp
+++ kukukuku/main.cpp
@@ -1,32 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct table
-{
-    int data;
-    struct table* next;
-};
-int main()
-{
-    int n=0,i=0;
-    struct table *L;
-    struct table *t;
-    L=(struct table*)malloc(sizeof(struct table));
-    L->next=NULL;
-    t=(struct table*)malloc(sizeof(struct table));
-    scanf("%d",&t->data);
-    while(t->data!=-1)
-    {
-        if((t->data%2)!=0)
-        {
-            n++;
-        }
-        scanf("%d",&t->data);
-    };
-   t=L->next;
-   while(t!=NULL)
-    {
-        t=t->next;
-        printf("%d",t->data);
-    };
+#include <stdio.h>
+#define N 1000
+int arr[N];
+/* 对长度为n的数组arr执行插入排序 */
+void insertionSort(int arr[], int n);
+/* 打印长度为n的数组arr */
+void printArray(int arr[], int n);
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+    insertionSort(arr, n);
+    printArray(arr, n);
     return 0;
 }
+/* 打印长度为n的数组arr */
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1)  /* 下标0..n-2每个元素后面有个空格 */
+            printf(" ");/*下标n-1,也就是最后一个元素后面没有空格*/
+    }
+    printf("\n");/* 一行打印完后换行 */
+}
+void insertionSort(int arr[], int n)
+{
+    int i,t;
+    for(i=0;i<n;i++) //1 2 3
+    {
+        for(int j=i+1;j<n;j--)
+        {
+            t=arr[j];
+            if(arr[i]<arr[j])
+                //取代，插入；
+                
+        }
+    }
+}
+    
